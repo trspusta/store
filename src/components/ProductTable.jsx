4063ba1878ba77b7a09e2b5360f97da7db7a6af8
trspusta <trspusta@addu.edu.ph>
2024-09-17 19:04:41 +0800

    Change item name

--- src/components/ProductTable.jsx
+++ src/components/ProductTable.jsx
@@ -17,11 +17,11 @@ const ProductTable = () => {
                     <Table.ColumnHeader colspan="2">Sporting Goods</Table.ColumnHeader>
                 </Table.Row>
                 <Table.Row>
-                    <Table.Column>Football</Table.Column>
+                    <Table.Column>Tennis</Table.Column>
                     <Table.Column>$49.99</Table.Column>
                 </Table.Row>
                 <Table.Row>
-                    <Table.Column>Baseball</Table.Column>
+                    <Table.Column>Badminton</Table.Column>
                     <Table.Column>$9.99</Table.Column>
                 </Table.Row>
                 <Table.Row>
